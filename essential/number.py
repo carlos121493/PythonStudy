6d8c868cda2c8146b7b06755c5534c011f865564
carlos121493 <guowei@shnow.cn>
2014-10-04 17:45:20 +0800

    finished essential study

---
+++ essential/number.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python
+ret = raw_input('want to see number?(TRUE//FALSE)');
+if ret:
+    print 10%3
+else:
+    print r'''hello
+/////world'''
---
+++ essential/statements.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python
+# *_* encoding: utf-8 *_*
+sum =0;
+for i in range(int(raw_input('please input range to sum'))):
+    sum += i;
+print sum;
